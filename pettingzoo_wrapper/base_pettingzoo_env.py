b4e0cb5acc7901210dd22eee9b3916c47ed979cd
Mark <mark.trovinger@gmail.com>
2025-06-16 08:11:11 -0400

    added several functions from base gym env, inventorying which will likely require updating for PZ

--- pettingzoo_wrapper/base_pettingzoo_env.py
+++ pettingzoo_wrapper/base_pettingzoo_env.py
@@ -38,6 +38,7 @@ class CustomEnvironment(ParallelEnv, EzPickle):
         self.game = vzd.DoomGame()
         self.game.load_config(level)
         self.game.set_window_visible(False)
+        self.render_mode = render_mode
 
         screen_format = self.game.get_screen_format()
         if (
@@ -49,7 +50,9 @@ class CustomEnvironment(ParallelEnv, EzPickle):
                 f" wrapper. Forcing RGB24."
             )
             self.game.set_screen_format(vzd.ScreenFormat.RGB24)
-        self.state = None
+        # this line errors, as state seems to be a protected variable name
+        #self.state = None
+        self._state = None
         self.clock = None
         self.window_surface = None
         self.isopen = True
@@ -82,18 +85,119 @@ class CustomEnvironment(ParallelEnv, EzPickle):
         self._action_space = self.action_space()
 
         # specify observation space(s)
-        self.__observation_space = self.observation_space()
+        self._observation_space = self.observation_space()
 
         self.game.init()
 
     def reset(self, seed=None, options=None):
-        pass
+        super().reset(seed=seed)
+        if seed is not None:
+            self.game.set_seed(seed)
+        self.game.new_episode()
+        self.state = self.game.get_state()
 
-    def step(self, actions):
-        pass
+        return self.__collect_observations(), {}
+
+    def step(self, action):
+        assert self.action_space.contains(
+            action
+        ), f"{action!r} ({type(action)}) invalid"
+        assert self.state is not None, "Call `reset` before using `step` method."
+
+        env_action = self.__build_env_action(action)
+        reward = self.game.make_action(env_action, self.frame_skip)
+        self.state = self.game.get_state()
+        terminated = self.game.is_episode_finished()
+        truncated = False  # Truncation to be handled by the TimeLimit wrapper
+        if self.render_mode == "human":
+            self.render()
+        return self.__collect_observations(), reward, terminated, truncated, {}
 
     def render(self):
         pass
+
+    def __parse_binary_buttons(self, env_action, agent_action):
+        if self.num_binary_buttons != 0:
+            if self.num_delta_buttons != 0:
+                agent_action = agent_action["binary"]
+
+            if np.issubdtype(type(agent_action), np.integer):
+                agent_action = self.button_map[agent_action]
+
+            # binary actions offset by number of delta buttons
+            env_action[self.num_delta_buttons :] = agent_action
+
+    def __parse_delta_buttons(self, env_action, agent_action):
+        if self.num_delta_buttons != 0:
+            if self.num_binary_buttons != 0:
+                agent_action = agent_action["continuous"]
+
+            # delta buttons have a direct mapping since they're reorganized to be prior to any binary buttons
+            env_action[0: self.num_delta_buttons] = agent_action
+
+    def __build_env_action(self, agent_action):
+        # MT: function copied from base gym env, may require updating
+        # encode users action as environment action
+        env_action = np.array(
+            [0 for _ in range(self.num_delta_buttons + self.num_binary_buttons)],
+            dtype=np.float32,
+        )
+        self.__parse_delta_buttons(env_action, agent_action)
+        self.__parse_binary_buttons(env_action, agent_action)
+        return env_action
+
+    def __collect_observations(self):
+        # probably need to update this per agent; we would have a loop that
+        # iterates over the agents and returns the observation/agent
+        observation = {}
+        if self.state is not None:
+            observation["screen"] = self.state.screen_buffer
+            if self.channels == 1:
+                observation["screen"] = self.state.screen_buffer[..., None]
+            if self.depth:
+                observation["depth"] = self.state.depth_buffer[..., None]
+            if self.labels:
+                observation["labels"] = self.state.labels_buffer[..., None]
+            if self.automap:
+                observation["automap"] = self.state.automap_buffer
+                if self.channels == 1:
+                    observation["automap"] = self.state.automap_buffer[..., None]
+            if self.num_game_variables > 0:
+                observation["gamevariables"] = self.state.game_variables.astype(
+                    np.float32
+                )
+        else:
+            # there is no state in the terminal step, so a zero observation is
+            # returned instead
+            for space_key, space_item in self.observation_space.spaces.items():
+                observation[space_key] = np.zeros(
+                    space_item.shape, dtype=space_item.dtype
+                )
+
+        return observation
+
+    def __parse_available_buttons(self):
+        """
+        Parses the currently available game buttons,
+        reorganizes all delta buttons to be prior to any binary buttons
+        sets ``num_delta_buttons``, ``num_binary_buttons``
+        """
+        delta_buttons = []
+        binary_buttons = []
+        for button in self.game.get_available_buttons():
+            if vzd.is_delta_button(button) and button not in delta_buttons:
+                delta_buttons.append(button)
+            else:
+                binary_buttons.append(button)
+        # force all delta buttons to be first before any binary buttons
+        self.game.set_available_buttons(delta_buttons + binary_buttons)
+        self.num_delta_buttons = len(delta_buttons)
+        self.num_binary_buttons = len(binary_buttons)
+        if delta_buttons == binary_buttons == 0:
+            raise RuntimeError(
+                "No game buttons defined. Must specify game buttons using `available_buttons` in the "
+                "config file."
+            )
 
     def __get_continuous_action_space(self):
         """
@@ -106,7 +210,9 @@ class CustomEnvironment(ParallelEnv, EzPickle):
             dtype=np.float32,
         )
 
-    @functools.lru_cache(maxsize=None)
+    # PZ wrapper tutorial mentions lru_cache for obs, note sure if we need to do 
+    # this, or even should
+    #@functools.lru_cache(maxsize=None)
     def observation_space(self):
         """
         Returns observation space: Dict with Box entry for each activated buffer:
